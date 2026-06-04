e929aee73ba49628e6e43832ffff7855e8e13b00
sinchana <sinchanagn98@gmail.com>
2026-06-04 12:15:13 +0530

    Create insights.py

---
+++ pages/insights.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.title("🧠 Agriculture Insights")
+
+st.markdown("""
+
+# 🌾 Deep Insights
+
+## Key Findings
+
+✅ Rice contains maximum dataset samples
+
+✅ Wheat images are balanced
+
+✅ Sugarcane has high texture variation
+
+✅ Jute dataset contains strong color features
+
+✅ CNN model performs very well
+
+---
+
+# 🚀 Future Improvements
+
+- Disease Detection
+- Yield Prediction
+- Weather Forecasting
+- Soil Analysis
+- Satellite Monitoring
+
+""")
